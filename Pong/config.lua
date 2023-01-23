5b50f04bbae94428d5a7c2688e6b6f0e80168fe3
Corvier <victorherrera1316@gmail.com>
2023-01-22 20:04:00 -0600

    Creando players y ball

---
+++ Pong/config.lua
@@ -0,0 +1,18 @@
+player_1 = {
+    Rell = "fill",
+    PosX = 10,
+    PosY = 200,
+    AnchX = 20,
+    AnchY = 80,
+    Speed = 100,
+}
+
+player_2 = {
+    Rell = "fill",
+    PosX = 200,
+    PosY = 200,
+    AnchX = 20,
+    AnchY = 80,
+    Speed = 100,
+}
+
--- Pong/main.lua
+++ Pong/main.lua
@@ -1,13 +1,75 @@
-function love.load()
+player_1 = {
+    Rell = "fill",
+    PosX = 10,
+    PosY = 200,
+    AnchX = 20,
+    AnchY = 80,
+    Speed = 100,
+}
 
+player_2 = {
+    Rell = "fill",
+    PosX = 770,
+    PosY = 200,
+    AnchX = 20,
+    AnchY = 80,
+    Speed = 100,
+}
+
+ball = {
+    Rell = "fill",
+    PosX = love.window.getWidth() / 2,
+    PosY = love.window.getHeight() / 2,
+    Radio = 10,
+}
+
+function love.load()
 
 end
 
 function love.update(dt)
+    if (love.keyboard.isDown('w')) then
+        player_1.PosY = player_1.PosY - (player_1.Speed * dt)
+    elseif (love.keyboard.isDown('s')) then
+        player_1.PosY = player_1.PosY + (player_1.Speed * dt)
+    end
 
+    if (love.keyboard.isDown('up')) then
+        player_2.PosY = player_2.PosY - (player_2.Speed * dt)
+    elseif (love.keyboard.isDown('down')) then
+        player_2.PosY = player_2.PosY + (player_2.Speed * dt)
+    end
 end
 
 function love.draw()
     -- Los parametros son ("Esta rellenado", PosX, PosY, AncX, AncY)
-    love.graphics.rectangle("fill", 10, 200, 20, 80);
+    love.graphics.rectangle(
+        player_1.Rell,
+        player_1.PosX,
+        player_1.PosY,
+        player_1.AnchX,
+        player_1.AnchY
+    );
+
+    love.graphics.rectangle(
+        player_2.Rell,
+        player_2.PosX,
+        player_2.PosY,
+        player_2.AnchX,
+        player_2.AnchY
+    );
+
+    love.graphics.circle(
+        ball.Rell,
+        ball.PosX,
+        ball.PosY,
+        ball.Radio
+    )
+
+    love.graphics.circle(
+        "fill",
+        10,
+        10,
+        10
+    )
 end
