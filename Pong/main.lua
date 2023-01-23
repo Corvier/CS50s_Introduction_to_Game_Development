66d12a9ad8f69f50b60b330ba8f98496dddeaf39
Corvier <victorherrera1316@gmail.com>
2023-01-23 00:13:47 -0600

    Agregando rebote de la pelota

--- Pong/main.lua
+++ Pong/main.lua
@@ -1,12 +1,12 @@
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
-SPEED = 60
+SPEED = 150
 
 PLAYERS = {
     ONE = {
         mode = "fill",
         x = WINDOW_WIDTH - 30,
-        y = WINDOW_HEIGHT / 2,
+        y = WINDOW_HEIGHT / 2 + 10,
         width = 20,
         height = 100,
         speed = SPEED
@@ -15,7 +15,7 @@ PLAYERS = {
     TWO = {
         mode = "fill",
         x = WINDOW_WIDTH - WINDOW_WIDTH + 10,
-        y = WINDOW_HEIGHT / 2,
+        y = WINDOW_HEIGHT / 2 + 10,
         width = 20,
         height = 100,
         speed = SPEED
@@ -23,8 +23,13 @@ PLAYERS = {
 }
 
 BALL = {
-    Rell = "fill",
-    Radio = 10
+    mode = "fill",
+    radio = 20,
+    right_left = false,
+    up_down = false,
+    speed = 5,
+    x = WINDOW_WIDTH / 2,
+    y = WINDOW_HEIGHT / 2
 }
 
 function love.load()
@@ -38,18 +43,45 @@ function love.load()
 end
 
 function love.update(dt)
-    if (love.keyboard.isDown('w') and PLAYERS.ONE.y >= 0 ) then
-        PLAYERS.ONE.y = PLAYERS.ONE.y - (PLAYERS.ONE.speed * dt)
+    -- Para movimientos del jugador izquierdo
+    if love.keyboard.isDown('w') and (PLAYERS.TWO.y + PLAYERS.TWO.height) <= love.graphics.getHeight() then
+        PLAYERS.TWO.y = PLAYERS.TWO.y - (PLAYERS.TWO.speed * dt)
     elseif (love.keyboard.isDown('s')) then
+        PLAYERS.TWO.y = PLAYERS.TWO.y + (PLAYERS.TWO.speed * dt)
+    end
+
+    -- Para los movimientos del jugador derecho
+    if love.keyboard.isDown('up') and PLAYERS.ONE.y >= 0 then
+        PLAYERS.ONE.y = PLAYERS.ONE.y - (PLAYERS.ONE.speed * dt)
+    elseif (love.keyboard.isDown('down')) then
         PLAYERS.ONE.y = PLAYERS.ONE.y + (PLAYERS.ONE.speed * dt)
     end
     
-    if (love.keyboard.isDown('up') and (PLAYERS.TWO.y + PLAYERS.TWO.height) <= love.graphics.getHeight() ) then
-        PLAYERS.TWO.y = PLAYERS.TWO.y - (PLAYERS.TWO.speed * dt)
-    elseif (love.keyboard.isDown('down')) then
-        PLAYERS.TWO.y = PLAYERS.TWO.y + (PLAYERS.TWO.speed * dt)
+    -- Para el movimiento de la pelota
+    if (BALL.right_left) then
+        BALL.x = BALL.x + BALL.speed
+    else
+        BALL.x = BALL.x - BALL.speed
     end
-    
+    if (BALL.up_down) then
+        BALL.y = BALL.y - BALL.speed
+    else
+        BALL.y = BALL.y + BALL.speed
+    end
+
+    -- Para el rebote de la pelota
+    if (BALL.x + BALL.radio) >= WINDOW_WIDTH then
+        BALL.right_left = false;
+    end
+    if (BALL.x + BALL.radio) <= 0 then
+        BALL.right_left = true;
+    end
+    if (BALL.y + BALL.radio) >= WINDOW_HEIGHT then
+        BALL.up_down = true;
+    end
+    if (BALL.y + BALL.radio) <= 0 then
+        BALL.up_down = false;
+    end
 end
 
 function love.draw()
@@ -71,9 +103,9 @@ function love.draw()
     );
 
     love.graphics.circle(
-        "fill",
-        10,
-        10,
-        10
+        BALL.mode,
+        BALL.x,
+        BALL.y,
+        BALL.radio
     )
 end
